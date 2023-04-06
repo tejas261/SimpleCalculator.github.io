b69018469bc51fad846429cd5e8ddf1bd9b8de40
tejas261 <tejasmt884@gmail.com>
2023-04-06 21:04:11 +0530

    simple calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+    let screen =  document.querySelector('.screen');
+    let buttons =  document.querySelectorAll('.row1');
+
+    buttons.forEach(function(button){
+        button.addEventListener('click',calculate)
+    });
+
+    function calculate(event){
+        // displays the value in the box
+        let clickbutton = event.target.value;
+        if(clickbutton === '='){
+            if(screen.value!=''){
+                screen.value = eval(screen.value);
+            }
+        }else if(clickbutton=='AC'){
+            screen.value = ''
+        }else if(clickbutton=='C'){
+            let arr = screen.value;
+            let l=arr.length;
+            screen.value = arr.slice(0,l-1);
+        }
+        else{
+            screen.value += clickbutton;
+        }
+    }
